df762f8504e26ab9dd3bc976c8b7153ef1042e49
timkicker <tim.kicker@protonmail.com>
2023-08-13 21:30:31 +0200

    Update auto_submodule.sh

--- scripts/classic-blog-auto-pull/auto_submodule.sh
+++ scripts/classic-blog-auto-pull/auto_submodule.sh
@@ -14,12 +14,18 @@ git submodule update --recursive --remote
 
 # for updating
 update_submodule="git submodule update --recursive --remote";
+kill_container="docker kill `docker ps -aqf "name=classic-blog"`";
+start_container="docker compose up --build -d";
 
-cron_job="30 * * * * cd $repo_path && $update_submodule";
+cron_job="30 * * * * cd $repo_path && $update_submodule && $kill_container && $start_container;
 
 crontab -l | { cat; echo "$cron_job"; } | crontab -
 
 echo "git submodule will update every half hour in path $repo_path using the command $cron_job";
 
 fi
+
+
+
+
 
